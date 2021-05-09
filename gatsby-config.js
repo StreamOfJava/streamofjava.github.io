07ea3289e3d8a8819a5e3f1750e0b5f3d779c97f
Nicolai Parlog <nipa@codefx.org>
2021-05-09 11:45:36 +0200

    🗑️ Remove unnecessary files, add TypeScript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Stream<Java>`,
+    description: `Making it easy to find Java devs who live stream their work.`,
+    author: `Nicolai (nipafx)`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,18 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        title: `Stream<Java>`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FFFFFF`,
+        theme_color: `#C72A4A`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/streamofjava.svg`,
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+const IndexPage = () => (
+    <main>
+        <h1>Hi people</h1>
+    </main>
+)
+
+export default IndexPage
